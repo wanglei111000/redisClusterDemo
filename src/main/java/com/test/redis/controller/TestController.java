e7dd92323bd00ac6668b714a953f77537f671801
wanglei60 <wanglei60@lenovo.com>
2021-10-30 18:15:41 +0800

    测试swagger 成功

---
+++ src/main/java/com/test/redis/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.test.redis.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+
+@RestController(value = "/redis")
+public class TestController {
+
+    @ApiOperation(value="测试", notes="")
+    @GetMapping(value = "/test01" )
+//  @RequestMapping(value={""}, method= RequestMethod.GET)  //两种写法都可以
+   public void test01(){
+        System.out.println("============   测试swagger成功 ===================");
+    }
+}
